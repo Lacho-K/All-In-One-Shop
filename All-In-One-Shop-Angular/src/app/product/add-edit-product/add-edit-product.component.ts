acb7ad45259cef69573de217902e97581d09d401
Lachezar Kolev <lachezar.kolev.highschool@svetlina.net>
2022-11-26 17:42:22 +0200

    Editing fully functional

--- All-In-One-Shop-Angular/src/app/product/add-edit-product/add-edit-product.component.ts
+++ All-In-One-Shop-Angular/src/app/product/add-edit-product/add-edit-product.component.ts
@@ -16,49 +16,55 @@ export class AddEditProductComponent implements OnInit {
   constructor(private service: ShopApiService) { }
 
   
-  @Input() storageWithProduct:any;
+  @Input() storage:any;
   id:number = 0;
+  productQuantity:number = 0;
+  productLocation:string = '';
+  productRatings:string = '';
+
+  @Input() product:any;
   productId:number = 0;
   name:string = '';
   description:string = '';
   productImageURL:string = '';
   productTypeId!:number;
   price:number = 0;
-  productQuantity:number = 0;
-  productLocation:string = '';
-  productRatings:string = '';
   
   ngOnInit(): void {
-    this.id = this.storageWithProduct.id;
-    this.productId = this.storageWithProduct.productId;
-    this.name = this.storageWithProduct.name;
-    this.description = this.storageWithProduct.description;
-    this.productImageURL = this.storageWithProduct.productImageURL;
-    this.productTypeId = this.storageWithProduct.productTypeId;
-    this.price = this.storageWithProduct.price;
-    this.productQuantity = this.storageWithProduct.productQuantity;
-    this.productLocation = this.storageWithProduct.productLocation;
-    this.productRatings = this.storageWithProduct.productRatings;
+    this.id = this.storage.id;
+
+    //Using optional chaining (?.) operator to avoid any cannot read properties of 'undefined' errors
+    this.productId = this.product?.id;
+    this.name = this.product?.name;
+    this.description = this.product?.description;
+    this.productImageURL = this.product?.productImageURL;
+    this.productTypeId = this.product?.productTypeId;
+    this.price = this.product?.price;
+
+    this.productQuantity = this.storage.productQuantity;
+    this.productLocation = this.storage.productLocation;
+    this.productRatings = this.storage.productRatings;
 
     this.productLis$ = this.service.getProductsList();
     this.storageLis$ = this.service.getStoragesList();
     this.productTypesList$ = this.service.getProductTypesList();
   }
 
+  //TODO: Fix display of table when space in name of product
   addProduct(){
     var product = {
-      name: this.name,
-      description: this.description,
+      name: (this.name as String)?.trim(),
+      description: (this.description as String)?.trim(),
       productImageURL: this.productImageURL,
       productTypeId: this.productTypeId,
       price: this.price
-    }
+    }        
 
     this.service.addProduct(product).subscribe(lastAddedProduct => {
       var closeModalBtn = document.getElementById('add-edit-modal-close');
       
       var currentProductStorage = {
-        // casts last product to type 'any' and gets its id
+        // casts last added product to type 'any' and gets its id
         productId: (lastAddedProduct as any).id,
         productQuantity: this.productQuantity,
         productLocation: this.productLocation,
