26153d1da04acd9fc95822cb185b16951456b125
Lachezar Kolev <lachezar.kolev.highschool@svetlina.net>
2022-12-11 15:32:00 +0200

    Slide box displays items from database

--- All-In-One-Shop-Angular/src/app/home/home.component.ts
+++ All-In-One-Shop-Angular/src/app/home/home.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ShopApiService } from 'src/app/shop-api.service';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private service: ShopApiService) { }
+
+  recentProducts$!:Observable<any[]>;
+
 
   ngOnInit(): void {
+    this.recentProducts$ = this.service.getProductsList();    
   }
 
 }
