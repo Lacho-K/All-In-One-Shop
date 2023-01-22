2e5a93f1b7f2b349c5924a6fae8bb1a39a378e29
Lachezar Kolev <lachezar.kolev.highschool@svetlina.net>
2023-01-22 14:22:58 +0200

    Start implementing form validation for login

--- All-In-One-Shop-Angular/src/app/login/login.component.ts
+++ All-In-One-Shop-Angular/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,39 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+
+  loginForm !: FormGroup
+
+  constructor(private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.loginForm = this.fb.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    })
+  }
+
+  onSubmit(){
+    if(this.loginForm.valid){
+      console.log(this.loginForm.value);
+      
+    }
+    else{
+      this.validateAllFormField(this.loginForm)
+      
+    }
+  }
+
+  private validateAllFormField(formGroup : FormGroup){
+    Object.keys(formGroup.controls).forEach(field => {
+      const control = formGroup.get(field);
+      if(control instanceof FormControl){
+        control.markAsDirty({onlySelf: true});
+      }
+      else if(control instanceof FormGroup){
+        this.validateAllFormField(control);
+      }
+    })
   }
 
 }
